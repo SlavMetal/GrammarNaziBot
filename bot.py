505c001a13a01a643b8a7d53078901aa99dbacda
Max <7tc@protonmail.com>
2017-07-15 23:19:09 +0300

    Add private functions

--- bot.py
+++ bot.py
@@ -18,16 +18,16 @@
 # You should have received a copy of the GNU General Public License
 # along with GrammarNaziBot.  If not, see <http://www.gnu.org/licenses/>.
 
-# TODO add strings support
-# TODO add languages selection
-# TODO bold highlightning
-# TODO available only for one group and admins
-# because of API limits
+# TODO bold highlighting
+# TODO rewrite code of sending corrected words
+# TODO Functions for checks
 
 import logging
 import requests
 import yaml
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram import ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from functools import wraps
 
 # Open the config.yml file
 with open("config.yml", 'r') as ymlfile:
@@ -40,33 +40,66 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+# Functions with this decorator will work only in PM
+def private(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        if update.message.chat.type != 'private':
+            return
+        return func(bot, update, *args, **kwargs)
+    return wrapped
+
+
+@private
 def start(bot, update):
-    update.message.reply_text('Хой!')
+    update.message.reply_text('Hello there, {}! You can use me right here or add me to groups as well. '
+                              'I\'m actually private bot, see /help for more details. '
+                              .format(update.message.from_user.first_name))
 
 
+@private
 def help(bot, update):
-    update.message.reply_text('Help!')
+    update.message.reply_text('This bot checks grammar of every message sent in PM or group and '
+                              'it automatically detects English, Russian and Ukrainian words. '
+                              'Because of API limitations, this bot will NOT work for you or your groups, '
+                              'but you can use the source code to build your own `Grammar Nazi Bot`!\n\n'
+                              'Source code: https://github.com/SlavMetal/GrammarNaziBot\n'
+                              'Author: @SlavMetal', disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
 
 def echo(bot, update):
-    url = "http://speller.yandex.net/services/spellservice.json/checkText?text="
-    respond = requests.get(url + update.message.text)  # Get JSON data
+    url = "http://speller.yandex.net/services/spellservice.json/checkText?text=" + update.message.text
+    respond = requests.get(url)  # Get JSON data
     json_data = respond.json()  # Parsed JSON data
 
     corrected_words = ''  # Put corrected words here
+    chat = update.message.chat.id
+    word_entries = 0
 
-    for i in json_data:  # Add corrected words in array
-        arr = i['s']
-        if len(arr) == 1:
-            corrected_words += arr[0] + '; '
-        else:
-            brackets = ''
-            for j in range(1, len(arr)):
-                brackets += arr[j]
-                if j+1 != len(arr):  # Check if the element is not the last one
-                    brackets += ', '
-            corrected_words += arr[0] + ' (' + brackets + '?); '
-    update.message.reply_text(corrected_words, reply_to_message_id=update.message.message_id)
+    for i in json_data:
+        if i['s']:
+            word_entries += 1
+
+    if len(json_data) > 0 and chat in cfg['admins_ids'] or chat in cfg['groups_ids']:
+        for i in json_data:  # Adding corrected words to string
+            arr = i['s']  # Get corrected words in 'i' iteration
+            arr_length = len(arr)
+            if arr_length == 1:
+                corrected_words += arr[0] + '; '
+            elif arr_length > 1:  # If there are other possible corrected words
+                brackets = ''
+                for j in range(1, arr_length):
+                    brackets += arr[j]
+                    if not is_last(j, arr_length):
+                        brackets += ', '
+                corrected_words += arr[0] + ' ({}?); '.format(brackets)
+        update.message.reply_text(corrected_words, reply_to_message_id=update.message.message_id)
+
+
+def is_last(index: int, length: int):
+    if index + 1 != length:
+        return False
+    return True
 
 
 def error(bot, update, error):
@@ -80,7 +113,7 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
 
-    # on noncommand i.e message - echo the message on Telegram
+    # on no command
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
@@ -89,9 +122,7 @@ def main():
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # Run the bot until process receives SIGINT, SIGTERM or SIGABRT
     updater.idle()
 
 
